e8a351c384cdc22174c978f7423e96da15d9d500
Zai <130903099+zaibutcooler@users.noreply.github.com>
2025-02-19 11:05:20 +0630

    Remove models from .gitignore and update config

--- umi/config.py
+++ umi/config.py
@@ -1,8 +1,28 @@
 from torchvision import transforms
+from typing import Tuple
 
 
 class Config:
     epochs: int = 4
+
+    sample_size: int = 32
+    in_channels: int = 3
+    out_channels: int = 3
+    layers_per_block: int = 2
+    block_out_channels: Tuple = (128, 128, 256, 256)
+
+    down_block_types: Tuple = (
+        "DownBlock2D",
+        "DownBlock2D",
+        "AttnDownBlock2D",
+        "DownBlock2D",
+    )
+    up_block_types: Tuple = (
+        "UpBlock2D",
+        "AttnUpBlock2D",
+        "UpBlock2D",
+        "UpBlock2D",
+    )
 
     transform = transforms.Compose(
         [
